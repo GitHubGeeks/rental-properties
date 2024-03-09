9492f73630fc1b9a74ac957d2c829d6c9e9cd627
Julio Rocha <jc.roch@outlook.com>
2024-03-09 11:31:23 -0500

    Properties Page

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InfoBox = ({
     heading,
-    backgroundColor = ' bg-gray-100',
+    backgroundColor = 'bg-gray-100',
     textColor = 'text-gray-800',
     buttonInfo,
     children
@@ -14,8 +14,9 @@ const InfoBox = ({
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
-            <h2 className={`${textColor}text-2xl font-bold`}>{heading}</h2>
-            <p className={`${textColor}mt-2 mb-4`}>
+
+            <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+            <p className={`${textColor} mt-2 mb-4`}>
                     {children}
             </p>
             <a
@@ -23,19 +24,6 @@ const InfoBox = ({
               className={`inline-block ${buttonInfo.backgroundColor} text-white rounded-lg px-4 py-2 hover:opacity-80 `}
             >
               {buttonInfo.text}
-            </a>
-          </div>
-          <div className="bg-blue-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Property Owners</h2>
-            <p className="mt-2 mb-4">
-              List your properties and reach potential tenants. Rent as an
-              airbnb or long term.
-            </p>
-            <a
-              href="/add-property.html"
-              className="inline-block bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600"
-            >
-              Add Property
             </a>
           </div>
         </div>
