0c59a614200fa58bf451861955c7319039f6d5a3
GitHubGeeks <flynuke@gmail.com>
2024-03-17 11:38:54 -0400

    . Property & User Models

--- components/PropertyCards.jsx
+++ components/PropertyCards.jsx
@@ -18,7 +18,7 @@ const PropertyCards = ({ property }) => {
     }
   }
   return (
-    <div class="bg-white rounded-xl shadow-md relative">
+    <div className="bg-white rounded-xl shadow-md relative">
 
     <Image
       src={`/images/properties/${property.images[0]}`}
@@ -26,37 +26,37 @@ const PropertyCards = ({ property }) => {
       height={0}
       width={0}
       sizes='100vw'
-      class="w-full h-auto rounded-t-xl"
+      className="w-full h-auto rounded-t-xl"
     />
     
-    <div class="p-4">
-      <div class="text-left md:text-center lg:text-left mb-6">
-        <div class="text-gray-600">{property.type}</div>
-        <h3 class="text-xl font-bold">{property.name}</h3>
+    <div className="p-4">
+      <div className="text-left md:text-center lg:text-left mb-6">
+        <div className="text-gray-600">{property.type}</div>
+        <h3 className="text-xl font-bold">{property.name}</h3>
       </div>
       <h3
-        class="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right"
+        className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right"
       >
        {getRateDisplay()}
       </h3>
 
-      <div class="flex justify-center gap-4 text-gray-500 mb-4">
+      <div className="flex justify-center gap-4 text-gray-500 mb-4">
         <p>
           <FaBath className="inline mr-2" /> {property.beds}{' '}
-          <span class="md:hidden lg:inline">Beds</span>
+          <span className="md:hidden lg:inline">Beds</span>
         </p>
         <p>
         <FaBed className="inline mr-2" /> { property.baths }{' '}
-          <span class="md:hidden lg:inline">Baths</span>
+          <span className="md:hidden lg:inline">Baths</span>
         </p>
         <p>
         <FaRulerCombined className="inline mr-2" /> 
-          { property.square_feet}{' '} <span class="md:hidden lg:inline">sqft</span>
+          { property.square_feet}{' '} <span className="md:hidden lg:inline">sqft</span>
         </p>
       </div>
 
       <div
-        class="flex justify-center gap-4 text-green-900 text-sm mb-4"
+        className="flex justify-center gap-4 text-green-900 text-sm mb-4"
       >
         { property.rates.nightly && (<p><FaMoneyBill className='inline mr-2'/>Nightly</p>) }
         { property.rates.weekly && (<p><FaMoneyBill className='inline mr-2'/>weekly</p>) }
@@ -64,17 +64,17 @@ const PropertyCards = ({ property }) => {
 
       </div>
 
-      <div class="border border-gray-100 mb-5"></div>
+      <div className="border border-gray-100 mb-5"></div>
 
-      <div class="flex flex-col lg:flex-row justify-between mb-4">
-        <div class="flex align-middle gap-2 mb-4 lg:mb-0">
+      <div className="flex flex-col lg:flex-row justify-between mb-4">
+        <div className="flex align-middle gap-2 mb-4 lg:mb-0">
         <FaMapMarker className= 'text-orange-700 inline mr-2'/>
         {' '}
-          <span class="text-orange-700">{property.location.city} {property.location.state}</span>
+          <span className="text-orange-700">{property.location.city} {property.location.state}</span>
         </div>
         <a
           href={`/properties/${property._id}`}
-          class="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+          className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Details
         </a>
